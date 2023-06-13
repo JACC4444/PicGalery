a2ba9a004f2043d59201c037ea8f0a3eb75c20e0
Jonathan Chavarria <jonathan.chavarria.1703@outlook.es>
2023-06-12 19:27:41 -0600

    version 1.0

--- PicGalery/Startup.cs
+++ PicGalery/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using PicGalery.Models;
 
 [assembly: OwinStartupAttribute(typeof(PicGalery.Startup))]
 namespace PicGalery
@@ -9,6 +12,54 @@ namespace PicGalery
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRoleAndUser();
         }
+
+        private void CreateRoleAndUser()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var rolManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!rolManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                rolManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "jonathan@unan.edu.ni";
+                user.Email = "jonathan@unan.edu.ni";
+                string PDW = "123456";
+
+                var chkUser = userManager.Create(user, PDW);
+                if (chkUser.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            if (!rolManager.RoleExists("freemium"))
+            {
+                var role = new IdentityRole();
+                role.Name = "freemium";
+                rolManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "osmar@outlook.es";
+                user.Email = "osmar@outlook.es";
+                string PDW = "123456";
+
+                var chkUser = userManager.Create(user, PDW);
+                if (chkUser.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "freemium");
+                }
+            }
+        }
+
+
+
+
     }
 }
